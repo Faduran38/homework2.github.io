a6e85d62d6155681eb07aa391ac08a94841ba5aa
Faduran38 <faduran@cougarnet.uh.edu>
2026-03-24 19:29:54 -0500

    Update mis3371hw2.js

--- mis3371hw2.js
+++ mis3371hw2.js
@@ -1,1 +1,10 @@
-
+function myFunction() {
+  let x = 
+    document.getElementById("ssn").value;
+  let text;
+  if (isNaN(x) || x < 9 || x > 9) {
+    text = "Input not valid"
+  }
+  else {
+    text ="Input valid";
+  }
